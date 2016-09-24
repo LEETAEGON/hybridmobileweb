7873b7fb6ccc413265bc0de76f0a73622784f89d
mmjazz@naver.com <mmjazz@naver.com>
2016-09-24 18:46:04 +0900

    Initial commit before page navigation

---
+++ src/js/viewModels/home.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define(['ojs/ojcore', 'knockout', 'jquery'],
+    function(oj, ko, $) {
+        function HomeViewModel() {
+        var self = this;
+        //self.handleActivated = function(info) { };
+        //self.handleAttached = function(info) { };
+        //self.handleBindingsApplied = function(info) { };
+        //self.handleDetached = function(info) { };
+        }
+    return new HomeViewModel();
+    }
+);
